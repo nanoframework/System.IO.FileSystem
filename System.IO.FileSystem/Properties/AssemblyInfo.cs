c469444af7e0b5a5a31f0d1b4a094fb8f762f196
josesimoes <jose.simoes@eclo.solutions>
2020-12-11 18:41:36 +0000

    Work CI-CD
    - Update nuspec.
    - Add logo and rename license.
    - Update mscorlib NuGet.
    - Add nbgv package and build.

--- System.IO.FileSystem/Properties/AssemblyInfo.cs
+++ System.IO.FileSystem/Properties/AssemblyInfo.cs
@@ -5,11 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("System.Io.FileSystem")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("System.Io.FileSystem")]
+[assembly: AssemblyTitle("nanoFramework.System.Io.FileSystem")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyProduct("nanoFramework.System.Io.FileSystem")]
 [assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
