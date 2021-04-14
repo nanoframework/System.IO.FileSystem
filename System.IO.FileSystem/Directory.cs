89e3e6cbc6f3e11c03e62da249c5163395ea46a0
Jeremi Jasiński <jeremi.jasinski@gc5.pl>
2021-04-14 09:23:09 +0200

    Added more methods in Directory class

--- System.IO.FileSystem/Directory.cs
+++ System.IO.FileSystem/Directory.cs
@@ -18,33 +18,52 @@ namespace System.IO
 
         public static string[] GetLogicalDrives()
         {
-            return null;
+            return new string[] { "D:\\" };
         }
 
         public static bool CreateDirectory(string path)
         {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException(nameof(path));
+            }
             CreateNative(path);
             return true;
         }
 
         public static void Delete(string path, bool recursive = false)
         {
-
+            DeleteNative(path);
         }
 
         public static bool Exist(string path)
         {
-            return false;
+            return ExistsNative(path);
         }
 
         public static string[] GetFiles(string path)
         {
-            return null;
+            return GetFilesNative(path);
         }
 
         public static string[] GetDirectories(string path)
         {
-            return null;
+            return GetDirectoriesNative(path);
+        }
+
+        public static DateTime GetCreationTime(string path)
+        {
+            return GetCreationTimeNative(path);
+        }
+
+        public static DateTime GetLastAccessTime(string path)
+        {
+            return GetLastAccessTimeNative(path);
+        }
+
+        public static DateTime GetLastWriteTime(string path)
+        {
+            return GetLastWriteTimeNative(path);
         }
 
         #endregion
@@ -54,7 +73,7 @@ namespace System.IO
         [Diagnostics.DebuggerStepThrough]
         [Diagnostics.DebuggerHidden]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        private static extern bool ExistsNative(string path, string fileName);
+        private static extern bool ExistsNative(string path);
 
         [Diagnostics.DebuggerStepThrough]
         [Diagnostics.DebuggerHidden]
@@ -69,17 +88,32 @@ namespace System.IO
         [Diagnostics.DebuggerStepThrough]
         [Diagnostics.DebuggerHidden]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        private static extern void GetFilesNative(string path);
+        private static extern string[] GetFilesNative(string path);
 
         [Diagnostics.DebuggerStepThrough]
         [Diagnostics.DebuggerHidden]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        private static extern void GetDirectoriesNative(string path);
+        private static extern string[] GetDirectoriesNative(string path);
 
         [Diagnostics.DebuggerStepThrough]
         [Diagnostics.DebuggerHidden]
         [MethodImpl(MethodImplOptions.InternalCall)]
         private static extern void GetLogicalDrivesNative(string path);
+
+        [Diagnostics.DebuggerStepThrough]
+        [Diagnostics.DebuggerHidden]
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern DateTime GetCreationTimeNative(string path);
+
+        [Diagnostics.DebuggerStepThrough]
+        [Diagnostics.DebuggerHidden]
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern DateTime GetLastAccessTimeNative(string path);
+
+        [Diagnostics.DebuggerStepThrough]
+        [Diagnostics.DebuggerHidden]
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        private static extern DateTime GetLastWriteTimeNative(string path);
 
         #endregion
     }
