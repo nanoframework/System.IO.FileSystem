37ce2cee3b3b703608b6190b6d2d199d1db97d6a
Robin Jones <robin@networkfusion.co.uk>
2021-08-05 20:20:45 +0100

    Inital implementation of nanoFrameowork.Storage. TODO: add classes to an nfproj.

---
+++ nanoFramework.Storage/Information.cs
@@ -0,0 +1,26 @@
+//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System;
+
+namespace nanoFramework.Storage
+{
+
+    /// <summary>
+    /// Information about the current storage device
+    /// </summary>
+    public static class Information
+    {    
+        //TODO: List of available storage devices
+
+        //TODO: Current storage device size
+
+        //TODO: Currently used space on storage size
+
+        //TODO: Current folder size
+
+        //TODO: Current file size
+    }
+}
