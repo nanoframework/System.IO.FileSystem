759da610c0c06733211bc71acce6eaf3217326d0
Robin Jones <networkfusion@users.noreply.github.com>
2022-07-28 01:30:26 +0100

    Add initial tests
    Using big brother implementation: https://github.com/dotnet/runtime/tree/main/src/libraries/System.IO.FileSystem.Primitives/tests

---
+++ System.IO.FileSystem/tests/System.IO.FileSystem.Tests/FileAttributesTests.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using nanoFramework.TestFramework;
+using System.IO;
+using System.IO.FileSystem;
+
+namespace System.IO.FileSystem.Tests
+{
+    [TestClass]
+    public static class FileAttributesTests
+    {
+        [TestMethod]
+        public static void ValueTest()
+        {
+            Assert.Equal(0x0001, (int)FileAttributes.ReadOnly);
+            Assert.Equal(0x0002, (int)FileAttributes.Hidden);
+            Assert.Equal(0x0004, (int)FileAttributes.System);
+            Assert.Equal(0x0010, (int)FileAttributes.Directory);
+            Assert.Equal(0x0020, (int)FileAttributes.Archive);
+            Assert.Equal(0x0040, (int)FileAttributes.Device);
+            Assert.Equal(0x0080, (int)FileAttributes.Normal);
+            Assert.Equal(0x0100, (int)FileAttributes.Temporary);
+            Assert.Equal(0x0200, (int)FileAttributes.SparseFile);
+            Assert.Equal(0x0400, (int)FileAttributes.ReparsePoint);
+            Assert.Equal(0x0800, (int)FileAttributes.Compressed);
+            Assert.Equal(0x1000, (int)FileAttributes.Offline);
+            Assert.Equal(0x2000, (int)FileAttributes.NotContentIndexed);
+            Assert.Equal(0x4000, (int)FileAttributes.Encrypted);
+            Assert.Equal(0x8000, (int)FileAttributes.IntegrityStream);
+            Assert.Equal(0x20000, (int)FileAttributes.NoScrubData);
+        }
+
+    }
+}
