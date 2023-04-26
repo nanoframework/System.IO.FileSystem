baf13851bb648a431a262769c2a93b1a53562bdb
Robin Jones <networkfusion@users.noreply.github.com>
2023-04-26 17:09:00 +0100

    Comment out unused attributes As nF does not (currently) use them.

--- Tests/System.IO.FileSystem.Tests/Primitives/FileAttributesTests.cs
+++ Tests/System.IO.FileSystem.Tests/Primitives/FileAttributesTests.cs
@@ -16,17 +16,17 @@ namespace System.IO.FileSystem.Tests.Primitives
             Assert.AreEqual(0x0004, (int)FileAttributes.System);
             Assert.AreEqual(0x0010, (int)FileAttributes.Directory);
             Assert.AreEqual(0x0020, (int)FileAttributes.Archive);
-            Assert.AreEqual(0x0040, (int)FileAttributes.Device);
-            Assert.AreEqual(0x0080, (int)FileAttributes.Normal);
-            Assert.AreEqual(0x0100, (int)FileAttributes.Temporary);
-            Assert.AreEqual(0x0200, (int)FileAttributes.SparseFile);
-            Assert.AreEqual(0x0400, (int)FileAttributes.ReparsePoint);
-            Assert.AreEqual(0x0800, (int)FileAttributes.Compressed);
-            Assert.AreEqual(0x1000, (int)FileAttributes.Offline);
-            Assert.AreEqual(0x2000, (int)FileAttributes.NotContentIndexed);
-            Assert.AreEqual(0x4000, (int)FileAttributes.Encrypted);
-            Assert.AreEqual(0x8000, (int)FileAttributes.IntegrityStream);
-            Assert.AreEqual(0x20000, (int)FileAttributes.NoScrubData);
+            //Assert.AreEqual(0x0040, (int)FileAttributes.Device);
+            //Assert.AreEqual(0x0080, (int)FileAttributes.Normal);
+            //Assert.AreEqual(0x0100, (int)FileAttributes.Temporary);
+            //Assert.AreEqual(0x0200, (int)FileAttributes.SparseFile);
+            //Assert.AreEqual(0x0400, (int)FileAttributes.ReparsePoint);
+            //Assert.AreEqual(0x0800, (int)FileAttributes.Compressed);
+            //Assert.AreEqual(0x1000, (int)FileAttributes.Offline);
+            //Assert.AreEqual(0x2000, (int)FileAttributes.NotContentIndexed);
+            //Assert.AreEqual(0x4000, (int)FileAttributes.Encrypted);
+            //Assert.AreEqual(0x8000, (int)FileAttributes.IntegrityStream);
+            //Assert.AreEqual(0x20000, (int)FileAttributes.NoScrubData);
         }
 
     }
