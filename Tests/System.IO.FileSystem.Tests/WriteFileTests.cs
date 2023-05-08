726d4c1681b14ec57aab5cc5e7151aca8ebc41e5
Robin Jones <networkfusion@users.noreply.github.com>
2023-05-08 21:19:58 +0100

    Add write file test skeleton.

---
+++ Tests/System.IO.FileSystem.Tests/WriteFileTests.cs
@@ -0,0 +1,35 @@
+﻿
+
+using nanoFramework.TestFramework;
+using System.Diagnostics;
+using System;
+using System.IO;
+using System.Threading;
+
+namespace System.IO.FileSystem.Tests
+{
+    [TestClass]
+    public static class WriteFileTests
+    {
+        [TestMethod]
+        public static void FileStreamSeek()
+        {
+            //var drive = "I:"
+            //var stream = new FileStream($"{drive}\testdata.bin", FileMode.Create, FileAccess.Write);
+            //var firstData = new byte[] { 1, 2, 3 };
+            //stream.Write(firstData, 0, firstData.Length);
+            //var secondData = new byte[] { 6, 7, 8 };
+            //stream.Write(secondData, 0, secondData.Length);
+            //firstData = new byte[] { 0, 2, 3 };
+            //stream.Seek(0, SeekOrigin.Begin);
+            //stream.Write(firstData, 0, firstData.Length);
+            //stream.Dispose();
+
+            //var data = new FileStream($"{drive}\testdata.bin", FileMode.Open, FileAccess.Read);
+            //Console.WriteLine(data.Length.ToString());
+            //data.Dispose();
+            Console.WriteLine("TODO: TEST SHOULD INCLUDE ME!");
+        }
+
+    }
+}
