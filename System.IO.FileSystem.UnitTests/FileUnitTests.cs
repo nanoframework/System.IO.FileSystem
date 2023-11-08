d9dd8c41187bce1ada16c0045ab0cb9360f25474
Cory Charlton <ccharlton@pobox.com>
2023-11-07 20:45:57 -0800

    Updating dependencies

---
+++ System.IO.FileSystem.UnitTests/FileUnitTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using nanoFramework.TestFramework;
+
+namespace System.IO.FileSystem.UnitTests
+{
+    [TestClass]
+    public class FileUnitTests
+    {
+        protected const string Root = @"I:\";
+        protected static readonly string SourceFile = $@"{Root}{nameof(FileUnitTests)}-Source.test";
+
+        protected static readonly byte[] BinaryContent = Encoding.UTF8.GetBytes(TextContent);
+        protected const string TextContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+    }
+}
