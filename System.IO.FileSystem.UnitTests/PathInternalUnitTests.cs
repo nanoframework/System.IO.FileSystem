1cb71b05d4789709ecf85998f273606d35e24c84
Cory Charlton <ccharlton@pobox.com>
2023-11-14 09:01:52 -0800

    Adding unit test for `PathInternal.IsValidDriveChar`

---
+++ System.IO.FileSystem.UnitTests/PathInternalUnitTests.cs
@@ -0,0 +1,19 @@
+﻿using nanoFramework.TestFramework;
+
+namespace System.IO.FileSystem.UnitTests
+{
+    [TestClass]
+    public class PathInternalUnitTests
+    {
+        [TestMethod]
+        public void IsValidDriveChar_returns_true()
+        {
+            var tests = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            foreach (var test in tests)
+            {
+                Assert.IsTrue(PathInternal.IsValidDriveChar(test), $"Case: {test}");
+            }
+        }
+    }
+}
