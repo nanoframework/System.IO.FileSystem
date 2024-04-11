96037f91db09dbda10fee11f2559606df823a2d1
josesimoes <jose.simoes@eclo.solutions>
2024-04-11 20:29:13 +0100

    Remove unused usings

--- System.IO.FileSystem/DirectoryInfo.cs
+++ System.IO.FileSystem/DirectoryInfo.cs
@@ -3,9 +3,6 @@
 // See LICENSE file in the project root for full license information.
 //
 
-using System.IO.System.IO;
-using System.Runtime.CompilerServices;
-
 namespace System.IO
 {
     /// <summary>
